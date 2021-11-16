cf983fb9154cded8bcc42ce33dedc0363b3245d9
Mike Kargin <gray_cloud@mail.ru>
2021-11-16 16:08:24 +0300

    add Email Service

--- src/infrastructure/Travel.Shared/Services/EmailService.cs
+++ src/infrastructure/Travel.Shared/Services/EmailService.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 using MimeKit;
 using MailKit.Net.Smtp;
 using Travel.Application.Common.Interfaces;
+using Microsoft.Extensions.Logging;
+using Travel.Application.Common.Exceptions;
 
 namespace Travel.Shared.Services
 {
     public class EmailService : IEmailService
     {
-
-
         public async Task SendAsync(EmailDto request)
         {
             try
@@ -20,12 +20,17 @@ namespace Travel.Shared.Services
                 var email = new MimeMessage
                 {
                     Sender = MailboxAddress.Parse(request.From ?? MailSettings.EmailFrom)
-                }
-            }
-            catch (Exception)
-            {
+                };
+                email.To.Add(MailboxAddress.Parse(request.To));
 
-                throw;
+
+
+                await smtp.DisconnectAsync(true);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.Message, ex);
+                throw new ApiException(ex.Message);
             }
         }
     }
