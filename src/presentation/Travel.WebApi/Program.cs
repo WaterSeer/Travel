df95fbc62294e5acc2e04b9d26713be7f61362d3
Mike Kargin <gray_cloud@mail.ru>
2021-11-25 16:57:53 +0300

    add Serilog

--- src/presentation/Travel.WebApi/Program.cs
+++ src/presentation/Travel.WebApi/Program.cs
@@ -1,11 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Formatting.Compact;
 
 namespace Travel.WebApi
 {
@@ -13,7 +18,31 @@ namespace Travel.WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var name = Assembly.GetExecutingAssembly().GetName();
+            Log.Logger = new LoggerConfiguration().MinimumLevel.Debug()
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Assembly", $"{name.Name}")
+                .Enrich.WithProperty("Assembly", $"{name.Version}")
+                .WriteTo.SQlite(Environment.CurrentDirectory + @"\Logs\log.db", restrictedToMinimumLevel: LogEventLevel.Information, storeTimestampInUtc: true)
+                .WriteTo.File(new CompactJsonFormatter(), Environment.CurrentDirectory + @"\Logs\Log.json", rollingInterval: RollingInterval.Day, restrictedToMinimumLevel: LogEventLevel.Information)
+                .WriteTo.Console()
+                
+                
+                .CreateLogger();
+
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
